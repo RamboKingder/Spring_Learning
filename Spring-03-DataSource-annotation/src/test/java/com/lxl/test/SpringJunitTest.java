77b7fbdc6dd0ce2226fb53c10c67d6b686adf2f7
RamboKingder <44106918+RamboKingder@users.noreply.github.com>
2022-01-05 20:12:00 +0800

    做了一点小修改

--- Spring-03-DataSource-annotation/src/test/java/com/lxl/test/SpringJunitTest.java
+++ Spring-03-DataSource-annotation/src/test/java/com/lxl/test/SpringJunitTest.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 // @ContextConfiguration("classpath:application-context.xml") 如果有配置文件就用这个
 @ContextConfiguration(classes = {SpringConfiguration.class})
@@ -20,4 +23,14 @@ public class SpringJunitTest {
     public void test(){
         userService.save();
     }
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    public void testDataSource() throws SQLException {
+        System.out.println("==========测试数据源的连接对象===========");
+        System.out.println(dataSource.getConnection());
+    }
+
 }
